e89aa83f751ef970663923113a57cc217b15430c
Lohen Yumnan <lohensana@gmail.com>
2018-03-21 20:36:34 +0530

    Project HotelManzana Added

---
+++ HotelManzana/HotelManzana/Model/model.swift
@@ -0,0 +1,45 @@
+//
+//  model.swift
+//  HotelManzana
+//
+//  Created by Lohen Yumnam on 15/02/18.
+//  Copyright © 2018 Lohen Yumnam. All rights reserved.
+//
+
+import UIKit
+
+struct Registration {
+    var firstName: String
+    var lastName: String
+    var emailAddress: String
+
+    var checkInDate: Date
+    var checkOutDate: Date
+    var numberOfAdults: Int
+    var numberOfChildren: Int
+
+    var roomType: RoomType
+    var wifi: Bool
+}
+
+struct RoomType: Equatable {
+    var id: Int
+    var name: String
+    var shortName: String
+    var price: Int
+    
+    static var all: [RoomType] {
+        return [RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+                RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+                RoomType(id: 2, name: "Penthouse Suite", shortName: "PHS", price: 309)]
+    }
+
+}
+
+extension RoomType {
+    //Equatable Protocol Implementation for RoomType
+    static func ==(lhs: RoomType, rhs: RoomType) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
