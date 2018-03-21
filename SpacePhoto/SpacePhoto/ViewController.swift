ad1fbdc52248d7c0060a2f849bd993060f5ff48b
Lohen Yumnan <lohensana@gmail.com>
2018-03-21 20:22:17 +0530

    Code Cleaning

--- SpacePhoto/SpacePhoto/ViewController.swift
+++ SpacePhoto/SpacePhoto/ViewController.swift
@@ -28,18 +28,18 @@ class ViewController: UIViewController {
         }
         
     }
-    
+
     func updateUI(with photoInfo: PhotoInfo){
         let task = URLSession.shared.dataTask(with: photoInfo.url) { (data, response, error) in
             if let data = data,
                 let image = UIImage(data: data){
-                
+
                 // Sending this block of code to main Queue
                     DispatchQueue.main.async {
                         self.title = photoInfo.title
                         self.descriptionLabel.text = photoInfo.description
                         self.imageView.image = image
-                    
+
                         if let copyright = photoInfo.copyright{
                             self.copyrightLabel.text = "Compyright: \(copyright)"
                         } else {
@@ -48,9 +48,10 @@ class ViewController: UIViewController {
                 }
             }
         }
-        
+
         task.resume()
     }
-    
+
+   
 }
 
