0b623ef49331e4017e615c694592f7dfba9cb59f
LohenYumnam <17508663+lohenyumnam@users.noreply.github.com>
2018-03-24 23:19:54 +0530

    VC and view added

---
+++ Restaurant/Restaurant/VControler/MenuItemDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by Lohen Yumnam on 24/03/18.
+//  Copyright © 2018 Lohen Yumnam. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
