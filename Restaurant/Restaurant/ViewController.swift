54476a6ec5f80c1881414b5e7bc20a87cd3db6ac
LohenYumnam <17508663+lohenyumnam@users.noreply.github.com>
2018-03-24 21:32:05 +0530

    Project Restaurant Added

---
+++ Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Lohen Yumnam on 24/03/18.
+//  Copyright © 2018 Lohen Yumnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
