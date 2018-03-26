4f37ea0149674fe44b74b685a86861411e155eb8
LohenYumnam <17508663+lohenyumnam@users.noreply.github.com>
2018-03-26 12:19:28 +0530

    Model setup

--- Restaurant/Restaurant/Models/IntermediaryModels.swift
+++ Restaurant/Restaurant/Models/IntermediaryModels.swift
@@ -11,3 +11,12 @@ import Foundation
 struct Categories: Codable {
     let categories: [String]
 }
+
+struct PreparationTime: Codable {
+    let prepTime: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case prepTime = "preparation_time"
+    }
+}
+
