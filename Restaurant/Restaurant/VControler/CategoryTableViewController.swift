684e3f8c279e0323bb020674011641bcb6cdb3a6
LohenYumnam <17508663+lohenyumnam@users.noreply.github.com>
2018-03-26 14:54:22 +0530

    Menu VC now working

--- Restaurant/Restaurant/VControler/CategoryTableViewController.swift
+++ Restaurant/Restaurant/VControler/CategoryTableViewController.swift
@@ -47,16 +47,16 @@ class CategoryTableViewController: UITableViewController {
         return cell
     }
     
-
-
-    /*
-    // MARK: - Navigation
+   // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "MenuSegue"{
+            let menuTableViewController = segue.destination as! MenuTableViewController
+            let index = tableView.indexPathForSelectedRow!.row
+            menuTableViewController.category = categories[index]
+        }
     }
-    */
+ 
 
 }
